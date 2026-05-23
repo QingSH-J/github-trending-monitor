daa0ac1c4da8df0108c3d704984214a9b2e407bf
qsy <qingshiyuu@gmail.com>
2026-05-23 17:26:38 +0800

    feat: add scheduled scraper every 6 hours

---
+++ src/main/java/com/example/github_trending_monitor/scheduler/TrendingScheduler.java
@@ -0,0 +1,35 @@
+package com.example.github_trending_monitor.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.github_trending_monitor.service.TrendingService;
+import com.nimbusds.jose.shaded.gson.internal.bind.TreeTypeAdapter;
+
+@Component
+public class TrendingScheduler {
+    
+    private static final Logger logger = LoggerFactory.getLogger(TrendingScheduler.class);
+    
+    private final TrendingService trendingService;
+
+    public TrendingScheduler(TrendingService trendingService) {
+        this.trendingService = trendingService;
+    }
+
+    // Run every 6 hours
+    @Scheduled(fixedDelay= 10000)
+    public void scrapeDaily(){
+        try {
+            logger.info("Starting scheduled trending repositories scrape...");
+            trendingService.scrapeAndSave("daily", "");
+            trendingService.scrapeAndSave("weekly", "");
+            trendingService.scrapeAndSave("monthly", "");
+            logger.info("Finished scheduled trending repositories scrape.");
+        } catch (Exception e) {
+            logger.error("Error during scheduled trending repositories scrape: ", e.getMessage());
+        }
+    }
+}
